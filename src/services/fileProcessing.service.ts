37671be39bc25a49e6ada79759a61df42845acbb
joaoBrRj91 <joao1991algoritmo@gmail.com>
2026-02-02 20:47:22 -0300

    Ajuste no fluxo de otimização de sermão baseado no em arquivo PDV

--- src/services/fileProcessing.service.ts
+++ src/services/fileProcessing.service.ts
@@ -1,19 +1,28 @@
+import * as pdfjsLib from "pdfjs-dist";
+import path from "path";
+
 class FileProcessingService {
   private pdfjs: any = null;
 
-  private async initPdfJs(): Promise<void> {
+  private initPdfJs(): void {
     if (!this.pdfjs) {
-      this.pdfjs = await import("pdfjs-dist");
-      this.pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${this.pdfjs.version}/pdf.worker.min.js`;
+      this.pdfjs = pdfjsLib;
+      const workerPath = path.join(
+        path.dirname(require.resolve("pdfjs-dist/package.json")),
+        "build",
+        "pdf.worker.min.js",
+      );
+      this.pdfjs.GlobalWorkerOptions.workerSrc = workerPath;
     }
   }
 
   async extractTextFromPDF(fileData: Uint8Array): Promise<string> {
     try {
-      await this.initPdfJs();
+      this.initPdfJs();
+
+      console.log("Load pdf modules");
       const pdf = await this.pdfjs.getDocument({ data: fileData }).promise;
       let fullText = "";
-
       for (let pageNum = 1; pageNum <= pdf.numPages; pageNum++) {
         const page = await pdf.getPage(pageNum);
         const textContent = await page.getTextContent();
@@ -28,7 +37,7 @@ class FileProcessingService {
       throw new Error(
         `Failed to extract text from PDF: ${
           error instanceof Error ? error.message : "Unknown error"
-        }`
+        }`,
       );
     }
   }
@@ -42,17 +51,24 @@ class FileProcessingService {
       throw new Error(
         `Failed to extract text from plain text file: ${
           error instanceof Error ? error.message : "Unknown error"
-        }`
+        }`,
       );
     }
   }
 
   async processFile(
-    fileData: number[] | Buffer | Uint8Array,
-    mimeType: string
+    fileData: Buffer | Uint8Array,
+    mimeType: string,
   ): Promise<string> {
-    const uint8Array =
-      fileData instanceof Uint8Array ? fileData : new Uint8Array(fileData);
+    let uint8Array: Uint8Array = fileData;
+    if (mimeType === "application/pdf") {
+      console.log("pdf");
+      uint8Array = new Uint8Array(
+        fileData.buffer,
+        fileData.byteOffset,
+        fileData.byteLength,
+      );
+    }
 
     if (mimeType === "application/pdf") {
       return this.extractTextFromPDF(uint8Array);
@@ -60,7 +76,7 @@ class FileProcessingService {
       return this.extractTextFromPlainText(uint8Array);
     } else {
       throw new Error(
-        `Unsupported file type: ${mimeType}. Supported types: application/pdf, text/plain`
+        `Unsupported file type: ${mimeType}. Supported types: application/pdf, text/plain`,
       );
     }
   }
